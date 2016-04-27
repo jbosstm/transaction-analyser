68fd3a0873c31a30cf2f3c2ffcba72005f8a4f83
Yuan Hu <yuhu@redhat.com>
2016-04-27 18:47:27 +0800

    NTA-89: Add a keyword filter https://issues.jboss.org/browse/NTA-89

---
+++ core/src/main/java/io/narayana/nta/logparsing/as8/filters/GetFilterKeywords.java
@@ -0,0 +1,40 @@
+package io.narayana.nta.logparsing.as8.filters;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+public class GetFilterKeywords {
+	
+	InputStream inputStream;
+	String filterKeywords;
+	 
+	public String getPropValues() {
+ 
+		try {
+			Properties prop = new Properties();
+			String propFileName = "filter.properties";			
+			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}	
+			
+			filterKeywords = prop.getProperty("keywords");
+			
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e){	
+			}
+		}
+		return filterKeywords;
+	}
+}
